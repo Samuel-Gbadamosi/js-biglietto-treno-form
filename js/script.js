c1e80f9a15f080f4f6a2d3afb743570d817821f9
samuel <damosisamuel@gmail.com>
2020-12-15 16:47:06 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var nomeInput = document.getElementById('nome').value;
+console.log(nomeInput);
+
+// var distanza = document.getElementById('km').value;
+// console.log(distanza);
+// var eta = document.getElementById('eta').value;
+// console.log(eta);
+//
+// var costoBiglietto = 0.21 * distanza;
+
+// document.getElementById("nome-passegero").innerHTML = nomeInput;
